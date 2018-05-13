9bfa35a8797d902a5517322f8a10290cf9532f35
koheiy <my.act07elangr@gmail.com>
2018-05-13 20:29:21 +0900

    init

--- src/main/java/co/jp/neo4jexample/controller/SampleController.java
+++ src/main/java/co/jp/neo4jexample/controller/SampleController.java
@@ -2,10 +2,13 @@ package co.jp.neo4jexample.controller;
 
 import co.jp.neo4jexample.repository.SampleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -15,9 +18,8 @@ public class SampleController {
     private SampleRepository repository;
 
     @GetMapping("/example/search/{word}")
-    public String search(@PathVariable() String word) {
-        Map<String, Object> map = repository.searchWord(word);
-        System.out.println(repository.searchWord(word));
-        return "";
+    public ResponseEntity<List<Map<String, Object>>> search(@PathVariable String word) {
+        List<Map<String, Object>> returnValue = repository.searchWord(word);
+        return new ResponseEntity<>(returnValue, HttpStatus.OK);
     }
 }
--- src/main/java/co/jp/neo4jexample/repository/SampleRepository.java
+++ src/main/java/co/jp/neo4jexample/repository/SampleRepository.java
@@ -5,10 +5,11 @@ import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Map;
 
 @Repository
 public interface SampleRepository extends Neo4jRepository<String, Long> {
     @Query("match (t:Tweet{tweet:{word}}) return t")
-    Map<String, Object> searchWord(@Param("word") String word);
+    List<Map<String, Object>> searchWord(@Param("word") String word);
 }
